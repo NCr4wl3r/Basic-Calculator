a07cd6f64f1a09fd27fbc7eeb63d071f55b3b479
NCr4wl3r <cr4wl3r@gmail.com>
2022-02-18 20:38:43 +0100

    reforge

--- static/app.js
+++ static/app.js
@@ -1,40 +1,66 @@
 const MAX_DIGIT = 9;
+const ADD_OP = "+";
+const SUBS_OP = "-";
+const MULTY_OP = "*";
+const DIVIDE_OP = "/";
+const NO_OP = "";
 
+let lastOperator = NO_OP;
 let valueCache = 0;
+// let screenCache = "";
 let keyCacheMem = "";
-let result = 0;
+let total = 0;
 
 const isMaxDigit = () => keyCacheMem.length === MAX_DIGIT;
 
-const updateCache = (value) => {
-  keyCacheMem += value;
-  writeScreen();
+const updateValueCache = (strCache) => (valueCache = parseInt(strCache));
+
+const updateKeyCache = (keyStr) => {
+  keyCacheMem += keyStr;
+  updateValueCache(keyCacheMem);
 };
 
 const delCache = () => {
   keyCacheMem = "";
   valueCache = 0;
-  result = 0;
+  total = 0;
   writeScreen();
+};
+
+// If total it's not supplyed, pick keyCacheMem
+const writeScreen = (screenCache = keyCacheMem) => {
+  screen.innerHTML = screenCache;
+};
+
+const applyLastOperation = () => {
+  if (lastOperator === ADD_OP) {
+    total = addNumbers(total, valueCache);
+  }
+};
+
+const addNumbers = (totalValue, memValue) => {
+  const addResult = totalValue + memValue;
+  return addResult;
 };
 
 const result = () => {
-  valueCache = parseInt(keyCacheMem);
+  applyLastOperation();
+  lastOperator = "";
+  writeScreen(total);
 };
 
-const writeScreen = () => {
-  screen.innerHTML = keyCacheMem;
-};
-
-const getNumberKey = (number) => {
+const getNumberKey = (numberStr) => {
   if (!isMaxDigit()) {
-    updateCache(number);
+    updateKeyCache(numberStr);
   }
-};
-const addNumbers = () => {
-  result += parseInt(keyCacheMem);
-  keyCacheMem = "";
   writeScreen();
+};
+
+const addNumbersEvent = () => {
+  if (lastOperator) {
+    result();
+  }
+  lastOperator = ADD_OP;
 };
 
 // From the array of buttons, get their innerHTML;
@@ -42,9 +68,9 @@ for (const key of keyBoardBtns) {
   key.addEventListener("click", getNumberKey.bind(this, key.innerHTML));
 }
 
-btnAdd.addEventListener("click", addNumbers);
 btnDel.addEventListener("click", delCache);
 btnResult.addEventListener("click", result);
-// btnSubstract.addEventListener("click");
-// btnMultiply.addEventListener("click");
-// btnDivide.addEventListener("click");
+btnAdd.addEventListener("click", addNumbersEvent);
+// btnSubstract.addEventListener("click", substractNumbersEvent);
+// btnMultiply.addEventListener("click", multiplyNumbersEvent);
+// btnDivide.addEventListener("click", divideNumbersEvent);
